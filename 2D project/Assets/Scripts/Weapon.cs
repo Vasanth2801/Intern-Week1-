9035fef007f47dc4fbd57ef832f5833158f4db22
Vasanth2801 <vasanthravichandran28@gmail.com>
2025-09-05 10:07:20 +0530

    Weapon Switch With SO
    Added another two weapons and updated with pressing keys for switching the weapons

--- 2D project/Assets/Scripts/Weapon.cs
+++ 2D project/Assets/Scripts/Weapon.cs
@@ -1,28 +1,64 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Weapon : MonoBehaviour
 {
     [Header("weaponData")]
-    public Weapondata weapon;
-
+    public Weapondata[] weapon;
+    int currentWeaponIndex = 0;
+    Weapondata currentWeapon;
     int currentMag;
     float nextTimeToFire;
 
+    [Header("UI")]
+    public TextMeshProUGUI weaponNameText;
 
     void Start()
     {
-        currentMag = weapon.magazineSize;
-        Debug.Log("Equipped weapon: " + weapon.weaponName);
+        EquipWeapon(0);
     }
 
-    
+
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        // Shoot Button Pressed (Left Mouse Button)
+        if (Input.GetMouseButtonDown(0))
         {
             Shoot();
         }
+        // For Changing weapons(1,2,3 keys)
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            EquipWeapon(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            EquipWeapon(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            EquipWeapon(2);
+        }
+    }
 
+        void EquipWeapon(int index)
+        {
+            if ((index < 0 || index >= weapon.Length))
+            {
+                return;
+            }
+
+            currentWeaponIndex = index;
+            currentWeapon = weapon[index];
+            currentMag = currentWeapon.magazineSize;
+
+            Debug.Log("Equipped" + currentWeapon.weaponName);
+            
+           UpdateUI(); // Update the UI with current weapon
+
+        }
+    
 
         void Shoot()
         {
@@ -33,14 +69,24 @@ public class Weapon : MonoBehaviour
 
             if(currentMag > 0)
             {
-                Debug.Log(weapon.weaponName +  "Fired" + weapon.damage + " damage dealt.");
+                Debug.Log(currentWeapon.weaponName +  "Fired" + currentWeapon.damage + " damage dealt.");
                 currentMag--;
-                nextTimeToFire = Time.time + 1f / weapon.fireRate;
+                nextTimeToFire = Time.time + 1f / currentWeapon.fireRate;
             }
             else
             {
                 Debug.Log("Out of ammo!");
             }
+        }
+
+    //Update the UI with current weapon information
+    void UpdateUI()
+    {
+        if(weaponNameText != null)
+        {
+            weaponNameText.text = $"weapon: {currentWeapon.weaponName} \n" +
+                                  $"Damage: {currentWeapon.damage} \n" +
+                                  $"Ammo: {currentMag}/{currentWeapon.magazineSize}";
         }
     }
 }
