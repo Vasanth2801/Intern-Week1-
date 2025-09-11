2c225b93e758b6380e369eec41b88f5083f65b71
Vasanth2801 <vasanthravichandran28@gmail.com>
2025-09-11 08:48:12 +0530

    GameManager Implementation

---
+++ 2D project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance { get; private set; }      //Singleton instance of GameManager
+
+    //Calls the method before Start()
+    void Awake()
+    {
+        //Singleton pattern implementation
+        if (instance != null && instance != this)       // If an instance already exists and it's not this one
+        {
+            Destroy(gameObject);                      //Destroy this instance
+            return;
+        }
+        else
+        {
+            instance = this;                             // Set this as the instance
+            DontDestroyOnLoad(gameObject);               //To prevent the GameManager from being destroyed on scene load
+            Time.timeScale = 1f;                        // Ensure game time is running
+        }
+    }
+
+
+    // Method to resume the game
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;        // Resume game time
+    }
+
+    // Method to restart the game
+    public void Restart()
+    {
+        SceneManager.LoadScene("Restart");         //Load the Scene named "Restart"
+    }
+}
---
+++ 2D project/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;                // Reference to the pause menu UI GameObject
+
+
+    // Method to resume the game
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);            // Hide the pause menu UI
+        GameManager.instance.ResumeGame();        // Call the ResumeGame method from GameManager to resume the game
+    }
+
+
+    // Method to restart the game
+    public void Restart()
+    {
+        GameManager.instance.Restart();              // Call the Restart method from GameManager to restart the game
+    }
+
+}
